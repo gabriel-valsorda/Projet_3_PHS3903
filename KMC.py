8bd2be39a4a27293947ce0c1c28f90419867527b
gabriel-valsorda <valsorda.gabriel@gmail.com>
2025-04-11 12:59:03 -0400

    KMC implementation

---
+++ KMC.py
@@ -0,0 +1,83 @@
+
+import numpy as np
+
+def KMC2D_Laurent(config, positions_surface, deltaE, kT, deltamu, pas_temps):
+    N=5
+    deltamu=5
+    T=295
+    kb=1.380649e-23
+    kbT=kb*T
+
+    for i in range(pas_temps):
+        # Étape 1 : Générer la liste des 2N événements possibles
+        #(site,0 = désorption ou 1 = adsorption)
+        typeEvnt=[0,1]
+        listeEvnt=[]
+
+        for site in range(N):
+            for adOuDes in typeEvnt:
+                listeEvnt.append((site,adOuDes))
+
+        # Étape 2 : Calcul des w de chaque événement
+        deltamu=-0.5
+        kT=0.6
+        deltaE=1
+
+        w_liste=[]
+        for i in listeEvnt:
+            if i[1]==0:
+                w=np.exp(deltamu/kT)
+                w_liste.append(w)
+            if i[1]==1:
+                w=np.exp(-deltaE/kT)
+                w_liste.append(w)
+        W=np.sum(w_liste)
+
+        # Étape 3 : Normalisation
+        w_normalisee=[w/W for w in w_liste]
+
+        # Étape 4 : On génère un nombre aléatoire en 0 et 1 et on choisit le premier événement qui a une proba cumulée supérieure à r.
+        nombre_r=np.random.rand()
+        somme=0
+        compteur=0
+        proba_cumulée=[]
+        for i in w_normalisee:
+            somme+=i
+            proba_cumulée.append(somme)
+            if somme>nombre_r:
+                break
+            compteur+=1
+        evnt=listeEvnt[compteur]
+        if evnt[1]==1:
+            print(f"L'événement est une adsorption au site {evnt[0]}")
+        if evnt[1]==0:
+            print(f"L'événement est une désorption au site {evnt[0]}")
+        
+
+        # Étape 5 : Nouvelle configuration
+        site_changement=evnt[0]
+
+        # Cas désorption
+        if evnt[1]==0:
+            config[site_changement][positions_surface[site_changement][1]-1]=None
+            positions_surface[site_changement][1]-=1
+        
+        # Cas adsorption
+        if evnt[1]==1:
+            # Si somme de position de surface et site changement et paire, alors le prochain est un 0
+            if (positions_surface[site_changement][1]+site_changement)%2==0:
+                config[site_changement][positions_surface[site_changement][1]]=1
+                positions_surface[site_changement][1]+=1
+            else:
+                config[site_changement][positions_surface[site_changement][1]]=0
+                positions_surface[site_changement][1]+=1
+
+
+
+        # Étape 6  : Assigner un temps
+        nombre_r_temps=np.random.rand()
+        deltatemps_reel=-1/(W*np.log(nombre_r_temps))
+
+        print(config)
+        
+    return config, surface_initiale, deltatemps_reel
