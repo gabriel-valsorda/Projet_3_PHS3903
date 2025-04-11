09f64a978a606d99564a8d44d03b681153401abb
gabriel-valsorda <valsorda.gabriel@gmail.com>
2025-04-11 12:49:53 -0400

    encore

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ import domain
 from KMC import KMC2D_Laurent
 
 # Definition du substrat
-
 x = 10
 y = 10
 
-grid = domain.create_grid(x,y)     # [hauteur, largeur]
+grid = domain.create_grid(x,y,5)     # [hauteur, largeur]
 
 # KMC2D_Laurent(grid, deltaE=1, kT=0.6, deltamu=-0.5,pas_temps=2)
 interpreter.plot_growth_2d(grid)
 # domain.interatomic_distance_2d(grid,0)
 
-
+ion = domain.find_ions(grid)
 print(grid)
-
+print(ion[1])
 # domain.save_grid(grid)
