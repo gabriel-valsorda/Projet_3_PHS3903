71d58174eaf6c580731b4e84843c6ad8636a542e
gabriel-valsorda <valsorda.gabriel@gmail.com>
2025-04-27 16:36:29 -0400

    1-diffusion

--- diffusion.py
+++ diffusion.py
@@ -1,24 +1,92 @@
 import numpy as np
+import domain
 
 
-def diffusion(grid, surface_positions, jump_energie, ):
-    """
-    Simulate the diffusion of atoms on a 2D grid.
+def probabilite_diffusion(l, l_max, E_l, kT, kT_iso):
+    alpha = E_l * (1/kT - 1/kT_iso)
 
-    Parameters:
-    - grid: 2D numpy array representing the grid.
-    - surface_positions: List of tuples representing the positions of atoms on the surface.
-    - delta_t: Time step for the simulation.
-    - diffusion_constant: Diffusion constant for the atoms.
-
-    Returns:
-    - Probability of diffusion
-    """
-    
-    nx, ny = grid.shape
-
-    # Create a copy of the grid to store new positions
-    new_grid = np.copy(grid)
+    # Probabilite
+    if l_max > 1000000:
+        P_l = (2**l - 1) * (1 - np.exp(-alpha)) * (1 - 2*np.exp(-alpha)) / (np.exp(alpha * (l - 1)))
+    else:    
+        P__l = (2**l - 1) * np.exp(-alpha*(l-1)) * (1-np.exp(-alpha)) * (1-2*np.exp(-alpha)) / (1 - np.exp(-alpha*l_max)*(2**(l_max+1)*(1 - np.exp(-alpha)) - (1 - 2*np.exp(-alpha))))
 
 
-    return 
+def Diffusion(surface, Evnt, gamma_0):
+    position = Evnt[0]
+    print(position)
+    norm = np.linalg.norm(position)
+    largeur = len(surface)
+    N = int(1000/largeur)
+    nx = position[0]
+    ny = surface[nx][1]-1
+
+    # Liste des sites disponible pour la diffustion
+    if sum(position)%2 == 0: # pair (Na)
+        site_dispo = [site for site in surface if sum(site)%2 == 0]
+    elif sum(position)%2 != 0: # impair (Cl)
+        site_dispo = [site for site in surface if sum(site)%2 != 0]
+    '''
+    Implementation de la periodicite?
+    Pas sur de l'implemente, car la probabilite prend compte des chemins de meme longueur
+    Aussi, il faut considerer que puisqu'on repete le cristal periodiquement, une etape de diffusion implique un diffusion periodique...  
+    '''
+
+    print(site_dispo)
+    distance_possible = []
+    for site in site_dispo:
+        if site == position:
+            continue
+        distance = (site[0]-position[0]) + (site[1]-position[1])
+        
+        if distance not in distance_possible:
+            distance_possible.append(distance)
+    print(distance_possible)
+
+    new_dist = []
+    for n in range(N):
+        for dist in distance_possible:
+            new_dist.append(dist + n*largeur)
+
+    print(len(new_dist))
+
+    E_l = 1
+    kT = 0.5
+    kT_iso = 0.6
+    prob = []
+    alpha = E_l * (1/kT - 1/kT_iso)
+    l_max = distance_possible[-1]
+
+    for l in distance_possible:    
+        prob.append( (2**l - 1) * np.exp(-alpha*(l-1)) * (1-np.exp(-alpha)) * (1-2*np.exp(-alpha)) / (1 - np.exp(-alpha*l_max)*(2**(l_max+1)*(1 - np.exp(-alpha)) - (1 - 2*np.exp(-alpha)))))
+    print(prob)
+
+
+import interpreter
+
+x = 10
+y = 20
+grid = domain.create_grid(x, y, 3)
+grid[3,3] = 1
+grid[2,4] = 1
+grid[5,3] = 1
+grid[8,4] = 1
+
+grid[2,3] = 0
+grid[8,3] = 0
+
+
+surface = domain.find_surface(grid)
+
+Diffusion(surface, ([0,3],1),0)
+
+
+
+
+
+interpreter.plot_growth_2d(grid)
+
+
+
+
+
--- main.py
+++ main.py
@@ -20,83 +20,8 @@ deltamu = liste_deltamu[5]
 
 nb_iterations = 2
 
-temps_1KMC = 0.5
-
-temps_estimer =  5 * (temps_1KMC * nb_pas_temps * (x/50)) * nb_iterations
-print(f"Temps estime: {temps_estimer} secondes")
 
 
 
 
-liste_moy_gamma_kbt=[]
-liste_moy_rugosite_kbt=[]
-liste_std_gamma_kbt=[]
-liste_std_rugosite_kbt=[]
-start_big = time.time()
-for param_kbT in liste_kT:
-    # print(param_kbT)
-    liste_gamma=[]
-    liste_rugosite=[]
-    # start = time.time()
-    for b in range(nb_iterations):
-        grid_f, deltatemps_reel, parametres = KMC2D(grid, param_kbT, deltamu, nb_pas_temps,gamma=True, rugosity=True)
-        # print(parametres)
-        gamma_iteration=parametres[0]
-        liste_gamma.append(gamma_iteration)
-
-        rugosite_iteration=parametres[1]
-        liste_rugosite.append(rugosite_iteration)
-    # end = time.time()
-    # print("Temps d'execution : ", end-start)
-
-    gamma_moy_param=np.mean(liste_gamma)
-    rugosite_moy_param=np.mean(liste_rugosite)
-
-    gamma_std_param=np.std(liste_gamma)
-    rugosite_std_param=np.std(liste_gamma)
-
-    liste_moy_gamma_kbt.append(gamma_moy_param)
-    liste_moy_rugosite_kbt.append(rugosite_moy_param)
-
-    liste_std_gamma_kbt.append(gamma_std_param)
-    liste_std_rugosite_kbt.append(rugosite_std_param)
-# end_big = time.time()
-# print("Temps d'execution total : ", end_big-start_big)
-
-liste_gamma_moy_mu=[]
-liste_rugosite_moy_mu=[]
-liste_gamma_std_mu=[]
-liste_rugosite_std_mu=[]
-
-for param_mu in liste_deltamu:
-    # print(param_mu)
-    liste_gamma=[]
-    liste_rugosite=[]
-    for b in range(nb_iterations):
-        # start = time.time()        
-
-        grid_f, deltatemps_reel, parametres = KMC2D(grid, kT, param_mu, nb_pas_temps,gamma=True, rugosity=True)
-
-        gamma_iteration=parametres[0]
-        liste_gamma.append(gamma_iteration)
-
-        rugosite_iteration=parametres[1]
-        liste_rugosite.append(rugosite_iteration)
-        # end = time.time()
-        # print("Temps d'execution : ", end-start)
-
-    gamma_moy_param=np.mean(liste_gamma)
-    rugosite_moy_param=np.mean(liste_rugosite)
-
-    gamma_std_param=np.std(liste_gamma)
-    rugosite_std_param=np.std(liste_gamma)
-
-    liste_gamma_moy_mu.append(gamma_moy_param)
-    liste_rugosite_moy_mu.append(rugosite_moy_param)
-
-    liste_gamma_std_mu.append(gamma_std_param)
-    liste_rugosite_std_mu.append(rugosite_std_param)
-end_big = time.time()
-print("Temps par KMC : ", (end_big-start_big)/(11+5))
-print("Temps d'execution total : ", (end_big-start_big))
 
